5369edf302ee1b823670c72620dc36bd7d35bb2f
AdityaRawEngg <aditya.shukla@raweng.com>
2020-12-17 11:36:28 +0530

    Procfile added

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 const blogRouter = require("./routes/blogRouter");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, resp) => {
+  resp.send("Welcome to blog server");
+});
 app.use("/blogs", blogRouter);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server Started on Port 3000");
 });
