286e3e44f2f843116101d1a7e91f6118c49490d7
AdityaRawEngg <aditya.shukla@raweng.com>
2021-01-11 15:49:06 +0530

    Added a middleware to resolve cors problem

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const blogRouter = require("./routes/blogRouter");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
-
+app.use((req, resp, next) => {
+  resp.header("Access-Control-Allow-Origin", "*");
+  resp.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  resp.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.get("/", (req, resp) => {
   resp.send("Welcome to blog server");
 });
 app.use("/blogs", blogRouter);
 
 app.listen(PORT, () => {
-  console.log("Server Started on Port 3000");
+  console.log(`Server Started on Port ${PORT}`);
 });
